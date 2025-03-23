15a4506e793be5193db9779f16765553f1eaa7d0
s1189750 <s1189750@student.windesheim.nl>
2025-03-23 11:43:42 +0100

    initial

---
+++ ultravisie/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-n44wp!7r4j&)c$s%#5)5sxmt1=txt_b1x^ha_ttfw&+ha%0u(t"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
